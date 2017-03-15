4221f432536a3431691bbcd76bf81f1dca012af7
McLoy <tkachenko.vlad@hotmail.com>
2017-03-15 20:49:14 +0200

    Kyiv Algorithms Club №47 Delaunay triangulation - update

--- src/main/java/ua/com/vtkachenko/ciklum/kyiv/algorythms/club47DelaunayTriangulation/task1/ConvexHullTriangulation.java
+++ src/main/java/ua/com/vtkachenko/ciklum/kyiv/algorythms/club47DelaunayTriangulation/task1/ConvexHullTriangulation.java
@@ -1,66 +1,66 @@
 package ua.com.vtkachenko.ciklum.kyiv.algorythms.club47DelaunayTriangulation.task1;
 
-import geometry.convexhull.GrahamScan;
-import geometry.dto.Point;
-import geometry.dto.Segment;
-import geometry.triangulation.AbstractTriangulation;
-import geometry.triangulation.Triangle;
-import geometry.triangulation.utils.Draw;
-
-import javax.swing.text.Segment;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class ConvexHullTriangulation extends AbstractTriangulation {
-
-    public ConvexHullTriangulation(List<Point> points) {
-        super(points);
-    }
-
-    public static void main(String[] args) {
-        Draw draw = new Draw();
-//        List<Point>
-    }
-
-    @Override
-    protected void triangulate(List<Point> points) {
-        Collectors.sort(points, (p1, p2) -> {
-            int compare = Double.compare(p1.y(), p2.y());
-            return compare == 0 ? Double.compare(p1.x(), p2.x()) : compare;
-        });
-        Triangle triangle = new TRiangle(points.get(0), points.get(1), points.get(2));
-        List<Segment> prevHull = new ArrayList<>();
-        prevHull.add(triangle.side(0));
-        prevHull.add(triangle.side(1));
-        prevHull.add(triangle.side(2));
-        addTriangle(triangle);
-        for (int i = 3; i < points.size(); i++) {
-            List<Segment> newHull = GrahamScan.convexHull(points.sublist(0, i+1));
-
-            for (int j = 0; j < prevHull.size(); j++) {
-
-                if (!prevHull.get(j).equals(newHull.get(j))) {
-                    Point newPoint = newHull.get(j).end();
-
-                    for (int k = j; k < prevHull.size() && !newHull.get(j + 2).equals(prevHull.get(k)); k++) {
-                        Segment prevSeg = prevHull.get(k);
-                        addTriangle(new Triangle(newPoint, prevSeg.start(), prevSeg.end()));
-                    }
-                    break;
-                }
-
-//                Segment prevSeg = prevHull.get(j);
-//                Point newPoint = newHull.get(j+1).end();
-//                while (!prevSeg.equals(newHull.get(j+2)) && j<prevHull.size()){
-//                    j++;
-////                    Segment newSeg = newHull.get(j);
-//                    prevSeg = prevHull.get(j);
-//                    addTriangle(new Triangle(newPoint, prevSeg.start(), prevSeg.end()));
+//import geometry.convexhull.GrahamScan;
+//import geometry.dto.Point;
+//import geometry.dto.Segment;
+//import geometry.triangulation.AbstractTriangulation;
+//import geometry.triangulation.Triangle;
+//import geometry.triangulation.utils.Draw;
+//
+//import javax.swing.text.Segment;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public class ConvexHullTriangulation extends AbstractTriangulation {
+//
+//    public ConvexHullTriangulation(List<Point> points) {
+//        super(points);
+//    }
+//
+//    public static void main(String[] args) {
+//        Draw draw = new Draw();
+////        List<Point>
+//    }
+//
+//    @Override
+//    protected void triangulate(List<Point> points) {
+//        Collectors.sort(points, (p1, p2) -> {
+//            int compare = Double.compare(p1.y(), p2.y());
+//            return compare == 0 ? Double.compare(p1.x(), p2.x()) : compare;
+//        });
+//        Triangle triangle = new TRiangle(points.get(0), points.get(1), points.get(2));
+//        List<Segment> prevHull = new ArrayList<>();
+//        prevHull.add(triangle.side(0));
+//        prevHull.add(triangle.side(1));
+//        prevHull.add(triangle.side(2));
+//        addTriangle(triangle);
+//        for (int i = 3; i < points.size(); i++) {
+//            List<Segment> newHull = GrahamScan.convexHull(points.sublist(0, i+1));
+//
+//            for (int j = 0; j < prevHull.size(); j++) {
+//
+//                if (!prevHull.get(j).equals(newHull.get(j))) {
+//                    Point newPoint = newHull.get(j).end();
+//
+//                    for (int k = j; k < prevHull.size() && !newHull.get(j + 2).equals(prevHull.get(k)); k++) {
+//                        Segment prevSeg = prevHull.get(k);
+//                        addTriangle(new Triangle(newPoint, prevSeg.start(), prevSeg.end()));
+//                    }
+//                    break;
 //                }
-            }
-            prevHull = newHull;
-        }
-
-    }
-}
+//
+////                Segment prevSeg = prevHull.get(j);
+////                Point newPoint = newHull.get(j+1).end();
+////                while (!prevSeg.equals(newHull.get(j+2)) && j<prevHull.size()){
+////                    j++;
+//////                    Segment newSeg = newHull.get(j);
+////                    prevSeg = prevHull.get(j);
+////                    addTriangle(new Triangle(newPoint, prevSeg.start(), prevSeg.end()));
+////                }
+//            }
+//            prevHull = newHull;
+//        }
+//
+//    }
+//}
